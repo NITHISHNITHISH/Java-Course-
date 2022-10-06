7225dab0bae8681bccbb9cc671f07e6d04718e60
Nithish <47587540+NithishNithish@users.noreply.github.com>
2022-10-06 19:54:18 +0530

    Add files via upload

---
+++ DataTypeDemo.java
@@ -0,0 +1,12 @@
+public class DataTypeDemo {
+	public static void main(String[] args) {	
+		int a ; 
+		a=10;
+		System.out.println(a);
+		int b;
+		b=50;
+		System.out.println(b);
+		int c = a;
+		System.out.print(c);
+	}
+}
