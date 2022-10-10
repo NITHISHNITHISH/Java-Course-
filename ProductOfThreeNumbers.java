74e50ed8c506b934508bc2049f0715eb07d5a5f5
Nithish <47587540+NithishNithish@users.noreply.github.com>
2022-10-10 22:23:44 +0530

    Add files via upload

---
+++ ProductOfThreeNumbers.java
@@ -0,0 +1,12 @@
+package first;
+
+public class ProductOfThreeNumbers {
+
+	public static void main(String[]args) {
+			int a = 20;
+			int b = 30;
+			int c = 2;
+			int d = a*b*c;
+			System.out.println(d);
+	}		
+}
