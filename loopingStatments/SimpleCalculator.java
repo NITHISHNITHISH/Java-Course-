d72007dfac5870124706f5594443d5724a0750fe
Nithish <47587540+NithishNithish@users.noreply.github.com>
2022-11-16 00:04:12 +0530

    Add files via upload

---
+++ loopingStatments/SimpleCalculator.java
@@ -0,0 +1,65 @@
+package loopingStatments;
+
+import java.util.Scanner;
+
+public class SimpleCalculator {
+
+	public static void main(String[] args) {
+
+		SimpleCalculator sc=new SimpleCalculator();
+		sc.Calc();
+		
+	}
+	
+	
+	
+	public void Calc() {
+		
+		double a,b,result;
+		char ch;
+		
+		char repeat = 'N';
+		do {
+		Scanner scan=new Scanner(System.in);
+		System.out.print("Enter your choice \n Addition(+)\n subtraction(-)\n multiplication(*)\n division(/)"
+				+ "\n modulo division(%)\n");
+		ch=scan.next().charAt(0);
+		System.out.print("first Number : ");
+		a=scan.nextDouble();
+		System.out.print("second Number: ");
+		b=scan.nextDouble();
+		
+		switch(ch) {
+			case ('+'):
+				result=a+b;
+				System.out.println("Addition of two numbers : "+result);
+				break;
+			case ('-'):
+				result=a-b;
+			System.out.println("Subtraction of two numbers : "+result);
+			   	break;
+			case ('*'):
+				result=a*b;
+			System.out.println("Multiplication of two numbers : "+result);
+				break;
+			case ('/'):
+				result=a/b;
+			System.out.println("Division of two numbers :"+result);
+				break;
+			case ('%'):
+				result=a%b;
+			System.out.println("Modulo Divion of two numbers :"+result);
+				break;
+			default :
+				System.out.println("Enter valid operator ");
+				break;
+		}
+		System.out.println("do you want to continue [Y/N]");
+		repeat=scan.next().charAt(0);
+		
+		}while(repeat  == 'Y');
+		System.out.println("-----THE END------");
+		
+	}
+
+}
