d202d758723610edf59e0fbb6ecf55be604ccc46
Nithish <47587540+NithishNithish@users.noreply.github.com>
2022-11-16 19:57:51 +0530

    Add files via upload

--- loopingStatments/ArmstrongNumber.java
+++ loopingStatments/ArmstrongNumber.java
@@ -6,15 +6,16 @@ public class ArmstrongNumber {
 
 	public static void main(String[] args) {
 
-		int num,result=0;
+		int num,result=0,temp;
 		Scanner scan =new Scanner(System.in);
+		System.out.println("Enter Number : ");
 		num=scan.nextInt();
-		
+		temp=num;
 		while(num>0) {
 			result =(int) (result+Math.pow((num%10),3));
 			num=num/10;
 		}
-		if(result == num) {
+		if(result == temp) {
 			System.out.println("Armstrong Number");
 		}
 		else
