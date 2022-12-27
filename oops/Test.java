322ea0d80e02d37f603ae383137c060e5933f896
Nithish <47587540+NithishNithish@users.noreply.github.com>
2022-12-27 19:55:23 +0530

    Add files via upload

---
+++ oops/Test.java
@@ -0,0 +1,19 @@
+package oops;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		Student mystu1=new Student();
+		
+		mystu1.name ="nithish";
+		mystu1.regNo=001;
+		mystu1.totalMarks=450;
+		
+		mystu1.percentageCalc();
+		
+		
+		
+	}
+
+}
